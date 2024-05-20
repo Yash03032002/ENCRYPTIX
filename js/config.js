41cb87ac5ad3c568a5d3949dc9da4ae437955df3
Yash dekate <ydekate03@gmail.com>
2024-05-20 17:57:55 +0530

    change config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_LNNMkgqgT', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2pc5h7ft1v16dibqkp70c7hqa1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_jqkguLhZr', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2t98trm8507ii00s3g3leu82o2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
