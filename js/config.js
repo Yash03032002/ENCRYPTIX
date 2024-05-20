e5a87dd5c3105c8e68995762dae2772d595c09d6
Yash dekate <ydekate03@gmail.com>
2024-05-20 18:50:40 +0530

    add api url

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://dxz2b858ph.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://9lsm5sdu7e.execute-api.us-east-1.amazonaws.com/Production' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
